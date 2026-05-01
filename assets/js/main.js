b42f90bc3c9505c6c4ed1e374108391517b21022
Pavel <ladinpavel784@gmail.com>
2026-05-01 20:49:28 +0500

    Initial commit: сайт репетитора готов

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burgerBtn');
+    const menu = document.getElementById('navMenu');
+
+    if(burger && menu) {
+        burger.addEventListener('click', () => {
+            menu.classList.toggle('active');
+            menu.classList.toggle('hidden'); // Для Tailwind
+            menu.classList.toggle('flex');   // Для Tailwind
+        });
+    }
+});
